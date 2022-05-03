254ba38011d6301e0c7507e14c33506c90624b37
ваше имя <llpsnkj@gmail.com>
2022-05-03 20:32:17 +0500

    dbFix

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 const { Sequelize } = require('sequelize')
+const config = require('config')
 
 
 
-module.exports = sequelize = new Sequelize('postgres://pcdcnfhcyxjiiv:e70351d63988789cf1d17c7846c5de061aaa9ca4570a26a097f99552dd0810a8@ec2-52-71-69-66.compute-1.amazonaws.com:5432/d69lg051olv4ea', {
+module.exports = sequelize = new Sequelize(config.get('urlDb'), {
     dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 const config = require('config')
-const port = process.env.port || config.get('port')
+const port = process.env.PORT || 8001
 
 
 app.listen(port, () => console.log(`Server started port: ${port}`))
